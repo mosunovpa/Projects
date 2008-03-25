88f34ad41a1a57a2b4db2d9f7d0eca90d259598a
mosunovpa <mosunovpa@users.noreply.github.com>
2008-03-25 11:27:25 +0000

    реорганизация трей-меню, меню Notes выскакивать по левой кнопке

--- notes/src/resource.h
+++ notes/src/resource.h
@@ -15,13 +15,15 @@
 #define ID_POPUP_EXIT                   40004
 #define ID_POPUP_HIDEALLNOTES           40005
 #define ID_Menu                         40006
+#define ID_POPUP_NOTES                  40007
+#define ID_NOTES_NEW                    40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        108
-#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
