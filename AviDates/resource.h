485bd855919e5bae50074f287b53c74740f9873e
mosunovpa <mosunovpa@users.noreply.github.com>
2009-01-11 10:56:30 +0000

    column click processing

--- AviDates/resource.h
+++ AviDates/resource.h
@@ -15,6 +15,8 @@
 #define IDC_DATES                       1006
 #define IDC_CACULATE                    1007
 #define IDC_SPIN_FADE                   1009
+#define IDC_                            1010
+#define IDC_EDIT                        1010
 
 // Next default values for new objects
 // 
@@ -22,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
